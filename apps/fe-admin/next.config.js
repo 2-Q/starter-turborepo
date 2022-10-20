0152cb48e5832500919bb6f811c53dfdc95ca742
2-Q <243dwiki.com@gmail.com>
2022-10-21 06:21:19 +0700

    create project

---
+++ apps/fe-admin/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const withTM = require('next-transpile-modules')(['ui']);
+const nextConfig = withTM({
+  reactStrictMode: true,
+  swcMinify: true,
+})
+
+module.exports = nextConfig
---
+++ apps/fe-student/pages/index.jsx
@@ -0,0 +1,10 @@
+import { Button } from "ui";
+
+export default function Home() {
+  return (
+    <div className='bg-red-300'>
+      <div>hello</div>
+      <Button />
+    </div>
+  )
+}
