54ba55ea075b8ffdbcc06bea6e966c69b0a09f69
2-Q <243dwiki.com@gmail.com>
2022-10-21 15:24:06 +0700

    init tailwindCSS

---
+++ apps/nextjs/pages/index.jsx
@@ -0,0 +1,13 @@
+import { Button } from "ui";
+
+export default function Home() {
+  return (
+    <div className='h-screen flex'>
+      <div className="m-auto text-center">
+        <div className="text-5xl font-extralight mb-2">hello world!</div>
+        <div className="max-w-[60rem] text-sm mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et laborum at sapiente animi explicabo commodi officia, voluptatibus iste distinctio maiores nisi numquam dolorum quisquam pariatur labore! Laboriosam veniam deleniti accusamus.</div>
+        <Button />
+      </div>
+    </div>
+  )
+}
---
+++ packages/ui/components/Button.tsx
@@ -0,0 +1,4 @@
+
+export const Button = () => {
+  return <button className="text-xs text-sky-500 border border-sky-500 px-6 py-1 rounded-full">Button</button>;
+};
